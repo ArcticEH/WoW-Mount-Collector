e53c1e1e2d58217876c7f0840f85c66f4ca25ff8
Brandon Kolar <bbkprogramming@gmail.com>
2021-04-01 14:56:15 -0400

    commented adapters

--- app/src/main/java/ca/mohawk/kolar/adapters/CollectionAdapter.java
+++ app/src/main/java/ca/mohawk/kolar/adapters/CollectionAdapter.java
@@ -20,21 +20,36 @@ import ca.mohawk.kolar.R;
 import ca.mohawk.kolar.database.MyDbHelper;
 import ca.mohawk.kolar.models.CollectionModel;
 
+/**
+ * ListView adapter used to display custom collection layout
+ */
 public class CollectionAdapter extends ArrayAdapter<CollectionModel> {
-
     public static String TAG = "==CollectionAdapter==";
 
+    // Reference to the list of collection model objects
     List list;
 
+    /**
+     * Constructor
+     * @param context the context called from
+     * @param objects the collection of collection models passed in
+     */
     public CollectionAdapter(@NonNull Context context, @NonNull List<CollectionModel> objects) {
         super(context, 0, objects);
+        Log.d(TAG, "CollectionAdapter()");
 
+        // Set list with objects
         list = objects;
     }
 
+    /**
+     * Used to get collection model associated with a position in listview
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Log.d(TAG, "getView()");
+
         // Get data for this position
         CollectionModel collectionModel = getItem(position);
 
@@ -51,46 +66,41 @@ public class CollectionAdapter extends ArrayAdapter<CollectionModel> {
         // Set text
         mountId.setText(Integer.toString(collectionModel.MountId));
         mountName.setText(collectionModel.name);
-
         dateAdded.setText(getContext().getString(R.string.collectionAdapter_dateAdded, collectionModel.Date));
 
         // Set on click for delete item
         convertView.findViewById(R.id.DeleteButton).setOnClickListener(v -> {
             Log.d(TAG, String.valueOf(collectionModel.MountId));
 
-            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    switch (which){
-                        case DialogInterface.BUTTON_POSITIVE:
-                            // Get database instance
-                            MyDbHelper mydbhelper = new MyDbHelper(getContext());
-                            SQLiteDatabase db = mydbhelper.getReadableDatabase();
+            // Create dialog interface to provide alert menu before deleting
+            DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        // Get database instance
+                        MyDbHelper mydbhelper = new MyDbHelper(getContext());
+                        SQLiteDatabase db = mydbhelper.getReadableDatabase();
 
-                            // Remove from collection
-                            db.delete(mydbhelper.MOUNT_TABLE, mydbhelper.MOUNT_ID + " = " + collectionModel.MountId, null);
+                        // Remove from collection
+                        db.delete(mydbhelper.MOUNT_TABLE, mydbhelper.MOUNT_ID + " = " + collectionModel.MountId, null);
 
-                            // Remove from list and notify of refresh
-                            list.remove(position);
-                            notifyDataSetChanged();
-                            break;
+                        // Remove from list and notify of refresh
+                        list.remove(position);
+                        notifyDataSetChanged();
+                        break;
 
-                        case DialogInterface.BUTTON_NEGATIVE:
-                            //No button clicked
-                            break;
-                    }
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        // No button clicked
+                        break;
                 }
             };
 
+            // Build and display alert menu
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
             builder.setMessage(R.string.collectionAdapter_deleteAlert).setPositiveButton(R.string.collectionAdapter_yes, dialogClickListener)
                     .setNegativeButton(R.string.collectionAdapter_no, dialogClickListener).show();
-
-
-
-
         });
 
+        // Finally, return the configured view
         return convertView;
     }
 
--- app/src/main/java/ca/mohawk/kolar/adapters/MountAdapter.java
+++ app/src/main/java/ca/mohawk/kolar/adapters/MountAdapter.java
@@ -1,6 +1,7 @@
 package ca.mohawk.kolar.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +16,29 @@ import java.util.List;
 import ca.mohawk.kolar.R;
 import ca.mohawk.kolar.models.MountResult;
 
+/**
+ * /**
+ *  * ListView adapter used to display custom mount result layout
+ *  */
 public class MountAdapter extends ArrayAdapter<MountResult> {
+    public static String TAG = "==MountAdapter==";
 
+    /**
+     * Constructor
+     */
     public MountAdapter(@NonNull Context context, @NonNull List<MountResult> objects) {
         super(context, 0, objects);
+        Log.d(TAG, "MountAdapter()");
     }
 
+    /**
+     * Used to get mount result model associated with a position in listview
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Log.d(TAG, "getView()");
+
         // Get data for this position
         MountResult mountResult = getItem(position);
 
@@ -40,6 +55,7 @@ public class MountAdapter extends ArrayAdapter<MountResult> {
         mountId.setText(Integer.toString(mountResult.id));
         mountName.setText(mountResult.name);
 
+        // Return configured view
         return convertView;
     }
 }
